a996fbb41b8606af18a84267e67e609fd4d2edbd
kakaranara <wahyukoco535@gmail.com>
2022-07-12 13:58:41 +0700

    fetch success (without pagination)

--- lib/third.dart
+++ lib/third.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:suitmedia_test/data/cubit/reqres_cubit.dart';
+import 'package:suitmedia_test/data/model/Reqres.dart';
 
 class ThirdScreen extends StatefulWidget {
   const ThirdScreen({Key? key}) : super(key: key);
@@ -24,19 +25,43 @@ class _ThirdScreenState extends State<ThirdScreen> {
               context.read<ReqresCubit>().getData();
               return CenteredIndicator();
             }
-            return Text("ok");
+
+            List<Reqres> dataState = [];
+            bool isLoading = false;
+            if (state is ReqresLoading) {
+              isLoading = true;
+            } else if (state is ReqresLoaded) {
+              dataState = state.data;
+            }
+
+            if (dataState.isEmpty) {
+              return Text("Empty");
+            }
+
+            return ListView.separated(
+              shrinkWrap: true,
+              physics: ScrollPhysics(),
+              itemCount: dataState.length,
+              itemBuilder: (c, i) {
+                final data = dataState[i];
+                return _card(fullName: "${data.first_name} ${data.last_name}");
+              },
+              separatorBuilder: (c, i) => const SizedBox(
+                height: 5,
+              ),
+            );
           },
         ),
       ),
     );
   }
 
-  Widget _card() {
+  Widget _card({required String fullName}) {
     return InkWell(
       onTap: () {},
       child: ListTile(
         leading: CircleAvatar(child: Image.asset("res/ic_photo.png")),
-        title: Text("First Name"),
+        title: Text("$fullName"),
         subtitle: Text("Afafaf"),
       ),
     );
